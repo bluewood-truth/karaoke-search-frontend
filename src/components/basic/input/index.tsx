bef88ded49919d7efecc60d14da7aaac1486c2c7
bluewood <bluewood.truth@gmail.com>
2021-09-12 18:49:51 +0900

    feat: add Input component template

---
+++ src/components/basic/input/index.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import {InputProps} from '../types';
+
+const Styled = (props: InputProps) => {
+  return {};
+};
+
+const options = {};
+
+const Input = styled('input', options)(Styled);
+
+export default Input;
--- src/components/basic/types.ts
+++ src/components/basic/types.ts
@@ -3,3 +3,5 @@ import React from 'react';
 export interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {}
+
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
