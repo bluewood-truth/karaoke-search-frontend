7fdbeb7a29de1569926271f9ecfffdf03731587f
bluewood <bluewood.truth@gmail.com>
2021-09-15 16:19:16 +0900

    feat: define units and theme for my custom components

---
+++ src/components/basic/theme.ts
@@ -0,0 +1,30 @@
+const white = '#fff';
+
+const theme = {
+  colors: {
+    main: {
+      light: '#FFDF72',
+      normal: '#FFDF72',
+      dark: '#F2AA2C',
+      contrast: white,
+    },
+    gray: {
+      light: '#CED3D7',
+      normal: '#83878A',
+      dark: '#575A5E',
+      contrast: white,
+    },
+    warning: {
+      normal: '#BE1F0B',
+      contrast: white,
+    },
+  },
+
+  shadows: {
+    none: 'none',
+    sm: '0px 5px 10px rgba(0, 0, 0, 0.12)',
+    lg: '0px 8px 30px rgba(0, 0, 0, 0.24)',
+  },
+};
+
+export default theme;
---
+++ src/components/basic/units.ts
@@ -0,0 +1,30 @@
+export const fontSizes = {
+  xxs: '0.7rem',
+  xs: '0.79rem',
+  sm: '0.889rem',
+  md: '1rem',
+  lg: '1.125rem',
+  xl: '1.266rem',
+  xxl: '1.424rem',
+};
+
+export const fontWeights = {
+  thin: 200,
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+};
+
+export const spacing = {
+  none: 0,
+  xxs: '0.125em',
+  xs: '0.25rem',
+  sm: '0.5rem',
+  md: '1rem',
+  lg: '2rem',
+  xl: '4rem',
+  xxl: '8rem',
+};
