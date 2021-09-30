75e93a77c3ddd92ee28d05ae6b7199593cab48ae
bluewood <bluewood.truth@gmail.com>
2021-09-30 12:37:59 +0900

    feat: add search result page

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,10 +1,27 @@
+import {Box} from 'components/basic';
 import PageWrapper from 'components/PageWrapper';
-import {Link} from 'react-router-dom';
+import useSearch from 'hooks/api/useSearch';
+import {useHistory} from 'react-router';
+import {SearchQuery} from 'types/domain';
+import parseQuery from 'utils/parseQuery';
 
 const SearchResult = () => {
+  const history = useHistory();
+  const query = parseQuery(history.location.search) as SearchQuery;
+  const {isLoading, data} = useSearch(query);
+
+  if (isLoading) {
+    return <Box>Loading...</Box>;
+  }
+
   return (
     <PageWrapper>
-      This is search result.<Link to={'/'}>home</Link>
+      {data.map((item, index) => (
+        <Box key={index}>
+          {item.songNumber} {item.karaoke} {item.title} {item.singer}
+        </Box>
+      ))}
+      <Box style={{display: 'block'}}></Box>
     </PageWrapper>
   );
 };
