0ad046e5024bc0773e0d3daefeeec5e21785c56a
bluewood <bluewood.truth@gmail.com>
2021-10-20 21:32:05 +0900

    feat: update song table markup structure

--- src/components/SongTable.tsx
+++ src/components/SongTable.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import {Song} from 'types/domain';
-import {Box} from './basic';
+import { Box } from './basic';
+import Flex from './basic/flex';
 import Stack from './basic/stack';
 import Loading from './Loading';
+
+const SongTableRow = (props: {song: Song}) => {
+  const {song} = props;
+  return (
+    <Flex className='tableRow' width='xl'>
+      <Box flex='1'>{song.karaoke}</Box>
+      <Box flex='1'>{song.songNumber}</Box>
+      <Box flex='5'>{song.title}</Box>
+      <Box flex='3'>{song.singer}</Box>
+    </Flex>
+  );
+};
 
 const SongTable = (props: {songList: Song[]; isLoading: boolean}) => {
   const {songList, isLoading} = props;
   return (
     <React.Fragment>
-      <Stack spacing='sm'>
+      <Flex className='tableHeader' width='xl' fontWeight='bold'>
+        <Box flex='1'>노래방</Box>
+        <Box flex='1'>곡 번호</Box>
+        <Box flex='5'>곡 제목</Box>
+        <Box flex='3'>가수</Box>
+      </Flex>
+      <Stack>
         {songList.map((song, index) => (
-          <Box key={index}>
-            {song.songNumber} {song.karaoke} {song.title} {song.singer}
-          </Box>
+          <SongTableRow song={song} key={index} />
         ))}
       </Stack>
       {isLoading && <Loading />}
